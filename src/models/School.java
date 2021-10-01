8f80eb5d0edacfa72cc72e0870c9e44f445288b7
Gideon Udoh <geediegramgu@gmail.com>
2021-10-01 17:40:00 +0100

    School added

--- src/models/School.java
+++ src/models/School.java
@@ -1,4 +1,15 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class School {
+    private long id;
+    private String name;
+    private String description;
+    private List <Classes> classes = new ArrayList<>();
+    private SchoolType type;
+    private List<Staff> staffs = new ArrayList<>();
+    private Address address;
+    private List <Students> students = new ArrayList<>();
 }
